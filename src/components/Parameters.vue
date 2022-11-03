bcd018eca4d7c0cc56ce8d8b87b89ab609b89821
Simon Legner <Simon.Legner@gmail.com>
2022-11-03 20:06:10 +0100

    Parameters: fetch in Astro

--- src/components/Parameters.vue
+++ src/components/Parameters.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2>Parameters</h2>
+  <h2>{{ props.parameters.length }} Parameter</h2>
   <table class="table">
     <tr>
       <th>Name</th>
@@ -9,15 +9,12 @@
     </tr>
     <ParameterRow v-for="parameter in parameters" :parameter="parameter" />
   </table>
-  <SourceFooter :url="url" />
 </template>
 
 <script setup lang="ts">
-import { API, ParameterMetadataModel } from "./openapi";
+import type { ParameterMetadataModel } from "./openapi";
 import ParameterRow from "./ParameterRow.vue";
-import SourceFooter from "./SourceFooter.vue";
-const url = API + "/station/current/tawes-v1-10min/metadata";
-const response = await fetch(url);
-const { parameters }: { parameters: ParameterMetadataModel[] } =
-  await response.json();
+const props = defineProps<{
+  parameters: ParameterMetadataModel[];
+}>();
 </script>
