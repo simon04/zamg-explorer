a7297510d20b6c6e32ddf1f62a73b13640afc09d
Simon Legner <Simon.Legner@gmail.com>
2023-01-06 21:49:35 +0100

    Open Source Licenses

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,28 @@
-import { defineConfig } from 'astro/config';
-import vue from '@astrojs/vue';
+import { defineConfig } from "astro/config";
+import vue from "@astrojs/vue";
+import mdx from "@astrojs/mdx";
+
+import { readFileSync } from "fs";
+
+const pkg = JSON.parse(readFileSync("./package.json", { encoding: "utf8" }));
+const dependencies = Object.keys(pkg.dependencies)
+  .map((dependency) =>
+    JSON.parse(
+      readFileSync(`node_modules/${dependency}/package.json`, {
+        encoding: "utf8",
+      })
+    )
+  )
+  .map(({ name, version, license, homepage }) => ({
+    name,
+    version,
+    license,
+    homepage,
+  }));
+process.env.VUE_APP_DEPENDENCIES = JSON.stringify(dependencies);
 
 // https://astro.build/config
 export default defineConfig({
-	// Enable Vue to support Vue components.
-	integrations: [vue()],
+  // Enable Vue to support Vue components.
+  integrations: [vue(), mdx()],
 });
---
+++ src/components/OpenSourceLicences.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="list-inline">
+    <li v-for="d in dependencies" :key="d.name" class="list-inline-item">
+      <a :href="'https://www.npmjs.com/package/' + d.name + '/v/' + d.version" target="_blank" rel="external noopener">
+        {{ d.name }}
+      </a>
+      {{ d.version }}
+      <span v-if="d.license"> ({{ d.license }})</span>
+    </li>
+  </ul>
+</template>
+
+
+<script async setup lang="ts">
+import { ref } from 'vue';
+const dependencies = ref(JSON.parse(import.meta.env.VUE_APP_DEPENDENCIES) as {
+  license: string;
+  name: string;
+  version: string;
+}[])
+</script>
