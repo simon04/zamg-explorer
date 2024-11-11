49f33bc2b2519cdeda27881d9425d361a7243dd9
Simon Legner <Simon.Legner@gmail.com>
2024-11-11 19:05:03 +0100

    Astro 4.16.10

--- src/components/TimeseriesStatistics.vue
+++ src/components/TimeseriesStatistics.vue
@@ -4,12 +4,14 @@
       Statistiken
     </caption>
     <thead>
+     <tr>
       <th>Station</th>
       <th>Parameter</th>
       <th class="text-end">Min</th>
       <th class="text-end">⌀</th>
       <th class="text-end">Max</th>
       <th class="text-end">Summe</th>
+     </tr>
     </thead>
     <tbody>
       <tr v-for="sp in stationsParameters">
